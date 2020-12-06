bb96379491b932a4d9cc7ca3f5a5c2417de0d3e6
mishka251 <mishkabelka251@gmail.com>
2020-12-07 03:24:14 +0500

    графики

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 
 from input_data import variant2
 from solvers import GradientSolver, GradientSplitSolver, CoordsSolver, Evristic, ConfigurationsConst, Simplex
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 solvers = [
     GradientSolver(alpha=0.3),
@@ -12,6 +13,33 @@ solvers = [
     Simplex(0.5),
 ]
 
+@np.vectorize
+def f(x1, x2):
+    return variant2.function(x1, x2)
+
 for solver in solvers:
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+
+
     res = solver(variant2)
     print(res)
+    X = np.arange(-5, 5, 0.25)
+    Y = np.arange(-5, 5, 0.25)
+    X, Y = np.meshgrid(X, Y)
+    Z = f(X,Y)
+
+    surf = ax.plot_surface(X, Y, Z,
+                           linewidth=0, antialiased=False, alpha=0.5)
+
+
+    l_x = [x[0] for x in res.path]
+    l_y = [x[1] for x in res.path]
+    l_z = [variant2.function(x, y) for (x, y) in res.path]
+    ax.plot(l_x, l_y, l_z, label='path')
+    ax.set_xlim(min(l_x)-1, max(l_x)+1)
+    ax.set_ylim(min(l_y) - 1, max(l_y) + 1)
+
+    #plt.show()
+    plt.savefig(res.method+'.jpg')
+    plt.clf()
